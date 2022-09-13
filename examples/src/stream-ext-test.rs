42ef1fda2ba3e42fc4b53d0860998c35252314e8
try-box <trybox@yeah.net>
2022-09-13 16:17:57 +0800

    Add test_with_leaky_bucket_limiter and test_with_governor_limiter

--- examples/src/stream-ext-test.rs
+++ examples/src/stream-ext-test.rs
@@ -2,24 +2,33 @@
 #![allow(dead_code)]
 
 use futures::{FutureExt, SinkExt, stream, StreamExt};
-use leaky_bucket::{AcquireOwned, RateLimiter};
+use futures::{Future, Stream};
+use rust_box::queue_ext::{QueueExt, Waker};
+use rust_box::stream_ext::{IntoLimiter, Limiter, LimiterExt};
 use std::collections::*;
+use std::fmt::Debug;
 use std::pin::Pin;
 use std::rc::Rc;
 use std::sync::Arc;
 use std::task::{Context, Poll};
 use std::time::Duration;
 use tokio::task::spawn_local;
-use try_box::stream_ext::{IntoLimiter, Limiter, LimiterExt};
-use try_box::queue_ext::{QueueExt, Waker};
+use tokio::time::{Instant, Sleep};
 
 fn main() {
     std::env::set_var("RUST_LOG", "stream_ext=info");
     env_logger::init();
 
     let runner = async move {
+        futures::future::join3(
+            test_with_limiter(),
+            test_with_leaky_bucket_limiter(),
+            test_with_governor_limiter(),
+        ).await;
+
         // test_with_limiter().await;
-        test_with_limiter2().await;
+        // test_with_leaky_bucket_limiter().await;
+        // test_with_governor_limiter().await;
     };
     // async_std::task::block_on(runner);
     tokio::task::LocalSet::new().block_on(&tokio::runtime::Runtime::new().unwrap(), runner);
@@ -27,21 +36,83 @@ fn main() {
 }
 
 async fn test_with_limiter() {
-    let s = stream::iter(0..10);
+    let s = stream::repeat(1);
 
-    let mut s = s.limiter(StreamRateLimiter::new());
+    const INTERVAL: Duration = Duration::from_millis(500);
+    #[derive(Debug)]
+    struct TheLimiter {
+        sleep: Pin<Box<Sleep>>,
+    }
+    ;
+    impl TheLimiter {
+        fn new() -> Self {
+            Self {
+                sleep: Box::pin(tokio::time::sleep(INTERVAL)),
+            }
+        }
+    }
+    impl Limiter for TheLimiter {
+        fn acquire(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<()>> {
+            match self.sleep.as_mut().poll(cx) {
+                Poll::Pending => Poll::Pending,
+                Poll::Ready(()) => {
+                    self.sleep.as_mut().reset(Instant::now() + INTERVAL);
+                    Poll::Ready(Some(()))
+                }
+            }
+        }
+    }
 
-    let mut count = 0;
+    let mut s = s.limiter(TheLimiter::new());
+
+    log::info!("stream limiter: {:?}", s);
+
     while let Some(item) = s.next().await {
-        count += 1;
-        log::info!("recv limiter: {:?}, count: {}", item, count);
+        log::info!("recv limiter: {:?}", item);
     }
 }
 
-async fn test_with_limiter2() {
+async fn test_with_leaky_bucket_limiter() {
+    // let s = get_queue_stream(20);
+    let s = stream::repeat(2);
+
+    let rate_limiter = leaky_bucket::RateLimiter::builder()
+        .initial(3)
+        .refill(1)
+        .interval(Duration::from_millis(500))
+        .max(100)
+        .fair(false)
+        .build();
+
+    let mut s = s.leaky_bucket_limiter(rate_limiter);
+
+    while let Some(item) = s.next().await {
+        log::info!("recv leaky_bucket_limiter: {:?}", item);
+    }
+}
+
+async fn test_with_governor_limiter() {
+    use futures::Stream;
+    use governor::state::StreamRateLimitExt;
+    use governor::{Quota, RateLimiter, RatelimitedStream};
+    use nonzero_ext::nonzero;
+
+    let s = stream::repeat(3);
+    // let s = get_queue_stream(10);
+    let lim = RateLimiter::direct(Quota::per_second(nonzero!(2u32)));
+    // let mut s = inner.ratelimit_stream(&lim);
+
+    let mut s = s.governor_limiter(&lim);
+
+    while let Some(item) = s.next().await {
+        log::info!("recv governor_limiter: {:?}", item);
+    }
+}
+
+fn get_queue_stream(max: i32) -> impl Stream<Item=i32> + Debug {
     use crossbeam_queue::SegQueue;
 
-    let s = Rc::new(SegQueue::default()).into_stream::<i32, _>(|s, _| {
+    let s = Rc::new(SegQueue::default()).queue_stream::<i32, _>(|s, _| {
         if s.is_empty() {
             Poll::Pending
         } else {
@@ -57,56 +128,10 @@ async fn test_with_limiter2() {
     });
 
     spawn_local(async move {
-        for i in 0..10 {
+        for i in 0..max {
             tx.send(i);
         }
     });
 
-    let mut s = s.limiter(StreamRateLimiter::new());
-
-    let mut count = 0;
-    while let Some(item) = s.next().await {
-        count += 1;
-        log::info!("recv limiter: {:?}, count: {}", item, count);
-    }
-}
-
-struct StreamRateLimiter {
-    inner: Arc<RateLimiter>,
-    acquire: Pin<Box<AcquireOwned>>,
-}
-
-impl Default for StreamRateLimiter {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl StreamRateLimiter {
-    fn new() -> Self {
-        let inner = Arc::new(
-            RateLimiter::builder()
-                .initial(3)
-                .refill(1)
-                .max(3)
-                .interval(std::time::Duration::from_millis(1000))
-                // .fair(false)
-                .build(),
-        );
-        let acquire = Box::pin(inner.clone().acquire_owned(1));
-        Self { inner, acquire }
-    }
-}
-
-impl Limiter for StreamRateLimiter {
-    fn acquire(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<()>> {
-        let poll = self.acquire.poll_unpin(cx);
-        if poll.is_ready() {
-            futures::ready!(poll);
-            self.acquire = Box::pin(self.inner.clone().acquire_owned(1));
-            Poll::Ready(Some(()))
-        } else {
-            Poll::Pending
-        }
-    }
+    s
 }
