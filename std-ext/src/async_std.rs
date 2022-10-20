cb056a9f7a4b1101b5e138e909f07f369d3ad176
try-box <trybox@yeah.net>
2022-10-20 22:14:50 +0800

    Add: RwLockExt, MutexExt

---
+++ std-ext/src/async_std.rs
@@ -0,0 +1,30 @@
+use async_lock::{RwLock, Mutex};
+
+impl<T: ?Sized> RwLockExt for T {}
+
+pub trait RwLockExt {
+
+    #[inline]
+    fn rwlock(self) -> RwLock<Self>
+        where
+            Self: Sized,
+    {
+        RwLock::new(self)
+    }
+
+}
+
+impl<T: ?Sized> MutexExt for T {}
+
+pub trait MutexExt {
+
+    #[inline]
+    fn mutex(self) -> Mutex<Self>
+        where
+            Self: Sized,
+    {
+        Mutex::new(self)
+    }
+
+}
+
